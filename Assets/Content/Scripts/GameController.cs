491fdbdb0fec1a744c86ef7d8cbb0a14ece5d70d
rinat <rrrinat84@gmail.com>
2020-09-30 11:43:58 +0300

    init commit

---
+++ Assets/Content/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Content.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        private Vector3 _direction = Vector3.right;
+        private float _speed = 5f;
+        
+        void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            var velocity = _direction * _speed;
+            transform.position = transform.position + velocity * Time.deltaTime;
+        }
+    }
+}
